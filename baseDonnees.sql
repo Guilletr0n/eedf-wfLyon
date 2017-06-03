09c15f0ce00bb8c35d6c6b965226450b26c232a2
CamilleDgn <c.digon@yahoo.com>
2017-06-03 16:24:02 +0200

    new BDD

--- baseDonnees.sql
+++ baseDonnees.sql
@@ -13,7 +13,8 @@ CREATE TABLE `photos` (
 	`date` DATE NOT NULL,
 	`photofilename` varchar(255) NOT NULL,
 	`size` INT(4) NOT NULL,
-	`id_categorie` INT(11) NOT NULL,
+	`id_gallery` INT(11) NOT NULL,
+	`photofile` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
@@ -77,6 +78,7 @@ CREATE TABLE `documents` (
 	`date` DATE NOT NULL,
 	`docfile` varchar(255) NOT NULL,
 	`size` INT(4) NOT NULL,
+	`docfilename` varchar(255) NOT NULL,
 	`id_categorie` INT(11) NOT NULL,
 	PRIMARY KEY (`id`)
 );
@@ -86,7 +88,7 @@ CREATE TABLE `documents` (
 /**************************************************************/
 
 ALTER TABLE `gallery` ADD INDEX(`id_event`);
-ALTER TABLE `photo` ADD INDEX(`id_event`);
+ALTER TABLE `photos` ADD INDEX(`id_gallery`);
 ALTER TABLE `events` ADD INDEX(`id_member`);
 ALTER TABLE `members` ADD INDEX(`id_section`);
 ALTER TABLE `members` ADD INDEX(`id_user`);
@@ -101,7 +103,7 @@ ALTER TABLE `Documents` ADD INDEX(`id_categorie`);
 ALTER TABLE `listemembers` ADD CONSTRAINT `listemembers_member` FOREIGN KEY (`id_member`) REFERENCES `members`(`id`);
 ALTER TABLE `listemembers` ADD CONSTRAINT `listemembers_event` FOREIGN KEY (`id_event`) REFERENCES `events`(`id`);
 ALTER TABLE `gallery` ADD CONSTRAINT `gallery_events` FOREIGN KEY (`id_event`) REFERENCES `events`(`id`);
-ALTER TABLE `photo` ADD CONSTRAINT `Photo_events` FOREIGN KEY (`id_event`) REFERENCES `events`(`id`);
+ALTER TABLE `photos` ADD CONSTRAINT `Photo_gallery` FOREIGN KEY (`id_gallery`) REFERENCES `gallery`(`id`);
 ALTER TABLE `members` ADD CONSTRAINT `member_users` FOREIGN KEY (`id_user`) REFERENCES `users`(`id`);
 ALTER TABLE `members` ADD CONSTRAINT `member_sections` FOREIGN KEY (`id_section`) REFERENCES `sections`(`id`);
 
@@ -114,3 +116,33 @@ INSERT INTO `sections` (`id`, `rank`) VALUES (NULL, 'Animateur'), (NULL, 'Parent
 INSERT INTO `users` (`username`, `email`, `phone`, `password`, `role`) VALUES ('nordine', 'nordine', '1', '$2y$10$iCI5Bmf5SlqF.oKuNt6feOW.OHOoC0772gmY3mVrQ46PhbgCGVg26', 'superadmin');
 INSERT INTO `users` (`username`, `email`, `phone`, `password`, `role`) VALUES ('fregevu', 'fregevuf@yahoo.fr', '0643163491', '$2y$10$cFMjZxLjZZHee0Jj7CRHWumafMGZyTshanTZH237f0SvvwW6t2mOW', 'admin');
 INSERT INTO `users` (`username`, `email`, `phone`, `password`, `role`) VALUES ('andre', 'estherandre07@orange.fr', '0', '$2y$10$cFMjZxLjZZHee0Jj7CRHWumafMGZyTshanTZH237f0SvvwW6t2mOW', 'adherent');
+
+/*********** Events ************/
+INSERT INTO `events` (`id`, `title`, `startdate`, `enddate`, `description`, `id_member`) VALUES
+(1, 'test', '2017-06-04', '2017-06-04', 'je suis pas d\'accord', 0),
+(2, 'Promenade champêtre', '2017-06-12', '2017-06-13', 'Promenons nous dans les bois', 1),
+(3, 'cache-cache géant', '2017-06-22', '2017-06-22', '8000 joueurs', 0),
+(4, 'pêche au canard', '2017-06-28', '2017-06-30', 'avec vrais faux canards', 2),
+(5, 'découverte arbres de la région', '2017-06-26', '2017-06-30', 'dégustation au top ^^', 5);
+
+/*********** Members ************/
+INSERT INTO `members` (`id`, `id_section`, `id_user`, `name`, `firstname`, `totem`, `infosup`, `register`) VALUES
+(2, 1, 1, 'Member1', 'Firstname1', 'totem1', 'infosup1', 1),
+(3, 2, 2, 'Member2', 'Firstname2', 'totem2', 'infosup2', 0),
+(4, 3, 3, 'Member3', 'Firstname3', 'totem3', 'infosup3', 1),
+(5, 4, 1, 'Member4', 'Firstname4', 'totem4', 'infosup4', 0),
+(6, 5, 2, 'Member5', 'Firstname5', 'totem5', 'infosup5', 1),
+(7, 6, 3, 'Member6', 'Firstname6', 'totem6', 'infosup6', 0);
+
+/*********** Documents ************/
+INSERT INTO `documents` (`id`, `docname`, `docdescription`, `date`, `docfile`, `size`, `id_categorie`, `docfilename`) VALUES
+(2, 'Fiche d\'adhésion 2017', 'Fiche à remplir avant chaque adhésion à EEDF Annonay', '2017-06-03', 'documents/adhesion-eedf-pdf', 367229, 1, 'Adhésion_EEDF.pdf'),
+(3, 'Fiche sanitaire 2017', 'Fiche à remplir avant chaque adhésion à EEDF Annonay', '2017-06-03', 'documents/fiche-sanitaire-auto-remplissable-pdf', 355263, 1, 'FICHE_SANITAIRE_auto_remplissable.pdf'),
+(4, 'Fiche de sortie Ainés', '22/23 octobre 2017', '2017-06-03', 'documents/convoc-aines-22-23-oct-aines-pdf', 537539, 2, 'convoc Ainés 22-23 oct Ainés.pdf'),
+(5, 'Fiche de sortie Eclaireurs', 'Sortie les 22/23 octobres 2017', '2017-06-03', 'documents/convoc-ecles-22-23-oct-ecles-pdf', 515875, 2, 'convoc Eclés 22-23 oct Eclés.pdf'),
+(6, 'Fiche de sortie Lutins - Louveteaux', 'Sortie les 22/23 octobres 2017', '2017-06-03', 'documents/convoc-ecles-22-23-oct-lutlvx-pdf', 414305, 2, 'convoc Eclés 22-23 oct LutLvx.pdf');
+
+-- --------------------------------------------------------
+
+
+/*********** Gallery ************/
