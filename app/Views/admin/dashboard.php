823f9aedae88d659d5ec952fee53d1e31ab1ba14
Guilletr0n <guillermo@gonzalez.rocks>
2017-06-04 10:44:14 +0200

    Update

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -16,7 +16,7 @@
 						</div>
 					</div>
 				</div>
-				<a href="<?= $this->url('document_documents');?>">
+				<a href="<?= $this->url('events_events_admin');?>">
 					<div class="panel-footer">
 						<span class="pull-left">Voir les détails</span>
 						<span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -143,4 +143,3 @@
 	</div>
 </div>
 <?php $this->stop('main_content') ?>
-
