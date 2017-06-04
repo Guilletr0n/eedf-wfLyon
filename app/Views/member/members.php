7ec788d1ecc15f68c402da3c0090efd3e448e9d3
Nowdine <Nordine.sebih@gmail.com>
2017-06-04 07:53:47 +0200

    ok

--- app/Views/member/members.php
+++ app/Views/member/members.php
@@ -2,16 +2,15 @@
 
 <?php $this->start('main_content') ?>
 
-	<div class="row">
-		<h3 class="text-center">Liste des adhérents</h3>
-		<p>Description</p> 
+	<div class="row" id="members">
+		<h3 class="text-center">Gestion des adhérent</h3>
 		<div id="listmember">
 			<div class="table-responsive">          
 					<table class="table">
 						<thead>
-							<tr>
-								<th>Section</th>
+							<tr class="info">
 								<th>id_user</th>
+								<th>Section</th>
 								<th>Nom</th>
 								<th>Prénom</th>
 								<th>Totem</th>
@@ -23,13 +22,13 @@
 			<?php foreach ($members as $key => $members) {
 				?>       
 						<tbody>
-							<tr>
-								<td><?php echo $members['id_section'] ?></td>
-								<td><?php echo $members['id_user'] ?></td>
-								<td><?php echo $members['name'] ?></td>
-								<td><?php echo $members['firstname'] ?></td>
-								<td><?php echo $members['totem'] ?></td>
-								<td><?php echo $members['infosup'] ?></td>
+							<tr class="active">
+								<td><?= $members['id_user'] ?></td>
+								<td><?= $listsections[$members['id_section']-1]['rank']?></td>
+								<td><?= $members['name'] ?></td>
+								<td><?= $members['firstname'] ?></td>
+								<td><?= $members['totem'] ?></td>
+								<td><?= $members['infosup'] ?></td>
 								<td><input type="checkbox" checked data-toggle="toggle" data-on="Ready" data-off="Not Ready" data-onstyle="success" data-offstyle="danger" value="0"></td>
 								<td><?= '
 				 		<a href="'. $this->url('member_editMember', ['id' => $members['id']]).'" class="btn btn-xs btn-success">Modifier</a>
