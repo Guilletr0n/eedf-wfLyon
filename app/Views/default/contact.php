45aa3812b21c8b58752d0fe1ab4424a6876b5c12
CamilleDgn <c.digon@yahoo.com>
2017-06-06 12:22:05 +0200

    CSS user

--- app/Views/default/contact.php
+++ app/Views/default/contact.php
@@ -5,10 +5,10 @@
 }?>
 <div class="container" id="containercontact">
 	<div class="row">
-		<h1 class="text-center bar">Où nous trouver ?</h1>
+		<h1 class="text-center apr">Où nous trouver ?</h1>
 	</div>
 	<div class="localisation">
-		<div class="col-md-6 bar" id="carte">
+		<div class="col-md-6 apr" id="carte">
 			<div class="adress">
 				<ul class="text-center">
 					<h2 class="text-center">Notre adresse</h2>	
@@ -22,7 +22,7 @@
 			<div class="text-center">
 			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2808.8714950085746!2d4.668762215457518!3d45.250391279099055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f513a54634ff03%3A0xfcc636aa6773d1e1!2s20+Chemin+de+la+Carri%C3%A8re%2C+07100+Annonay!5e0!3m2!1sfr!2sfr!4v1496587786295" width="500" height="400" frameborder="0" style="border:0" allowfullscreen></iframe></div>
 		</div>
-		<div class="col-md-5 bar" id="contact">
+		<div class="col-md-5 apr" id="contact">
 			<h2 class="text-center">Nous contacter</h2>
 			<form id="" method="post" action="<?= $this->url('default_contact2') ?>" enctype="multipart/form-data">
 				<div class="form-group">
