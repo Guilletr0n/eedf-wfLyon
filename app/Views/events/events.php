c3d76fa0a6a5615d86e224ce0cb4510db7e17bd7
TobyMechant <thibaut.roux89@gmail.com>
2017-06-06 10:15:20 +0200

    css

--- app/Views/events/events.php
+++ app/Views/events/events.php
@@ -1,14 +1,14 @@
 <?php $this->layout('EventsLayout', ['title' => 'Evénements']) ?>
 
 <?php $this->start('style') ?>
-<link rel="stylesheet" href="<?= $this->assetUrl('css/home.css') ?>">
+<link rel="stylesheet" href="<?= $this->assetUrl('css/events.css') ?>">
 <?php $this->stop('style') ?>
 
 <?php $this->start('main_content') ?>
 
     	<h2 class="text-center">Calendrier des événements</h2>
       <div class="container-fluid">
-          <div class="container text-center">
+          <div class="container text-center"id="calendar">
               <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTz=0&amp;mode=WEEK&amp;height=600&amp;wkst=2&amp;hl=fr&amp;bgcolor=%23FFFFFF&amp;src=eg295kbq80r81gn67nn6srj4eg%40group.calendar.google.com&amp;color=%235229A3&amp;ctz=Europe%2FParis" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
           </div>
       </div>
