9e2eebaaa9ce7514b0281b94f02789e1e62aaa52
TobyMechant <thibaut.roux89@gmail.com>
2017-06-06 11:18:47 +0200

    maj

--- app/Views/eventsLayout.php
+++ app/Views/eventsLayout.php
@@ -6,7 +6,7 @@
 	<meta name="Content-Type" content="UTF-8">
     <meta name="Content-Langage" content="fr">
 
-	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/style-user.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/events.css') ?>">
 	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/bootstrap.css') ?>" />
 	<link rel="shortcut icon" href="img/logo2.png">
 	<!-- Latest compiled and minified CSS -->
@@ -30,7 +30,7 @@
 			</div>
 		</div>
 
-		
+
 <!-- NAVBAR-->
 		<div class="container" id="nav">
 			<div class="col-md-12">
